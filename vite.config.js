9e92390ca65632342195cbd761a511cf44a6fcf6
morii21 <99586254+morii21@users.noreply.github.com>
2025-02-28 06:24:36 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure Vercel serves this folder
+    outDir: "dist",
+    assetsInlineLimit: 0,
   },
-  base: "/", // Ensures assets load correctly
 });
