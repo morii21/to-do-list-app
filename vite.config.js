91b71682d8279521d24c61ea0b5f5813aad79117
morii21 <99586254+morii21@users.noreply.github.com>
2025-02-28 06:34:56 +0800

    modify configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Add this line
   build: {
     outDir: "dist",
     assetsInlineLimit: 0,
